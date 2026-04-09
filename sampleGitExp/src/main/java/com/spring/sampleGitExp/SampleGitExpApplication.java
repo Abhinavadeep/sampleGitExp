297c3f65a6db5fb481411255001fcc8569e0bb78
Abhinavadeep <22jr1a0558@gmail.com>
2026-04-09 18:34:05 +0530

    Initial commit

---
+++ sampleGitExp/src/main/java/com/spring/sampleGitExp/SampleGitExpApplication.java
@@ -0,0 +1,13 @@
+package com.spring.sampleGitExp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleGitExpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleGitExpApplication.class, args);
+	}
+
+}
---
+++ sampleGitExp/src/test/java/com/spring/sampleGitExp/SampleGitExpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.sampleGitExp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleGitExpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
